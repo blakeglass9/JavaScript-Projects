3ae121f0be1007b6957e6f3c9d91f7333b1bf88e
blakeglass9 <146895621+blakeglass9@users.noreply.github.com>
2024-05-02 12:00:54 -0700

    ok

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+// Define two variables
+var sent1 = "This is the beginning of the string";
+var sent2 = " and this is the end of the string";
+
+// Concatenate the two variables
+var concatenated = sent1 + sent2;
+
+// Display the concatenated string in an alert
+window.alert(concatenated);
+
+// Write the concatenated string to the document
+document.write(concatenated);
+
+
+
+
+
