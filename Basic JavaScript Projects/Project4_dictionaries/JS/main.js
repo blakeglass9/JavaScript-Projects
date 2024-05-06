8463254e0cee29c29cf9c8c871a9a940ca7f468c
blakeglass9 <146895621+blakeglass9@users.noreply.github.com>
2024-05-06 14:21:13 -0700

    ok

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -12,18 +12,4 @@ var dictionary = {
 }
 
   
-  // Function to display a key value pair from the dictionary
-  function displayKeyValuePair() {
-    // Get a random key from the dictionary
-    var keys = Object.keys(dictionary);
-    var randomKey = keys[Math.floor(Math.random() * keys.length)];
-  
-    // Remove the key from the dictionary
-    var value = dictionary[randomKey];
-    delete dictionary[randomKey];
-  
-    // Display the key-value pair in the paragraph with id "dictionaryOutput"
-    var outputElement = document.getElementById("dictionaryOutput");
-    outputElement.textContent = "Key: " + randomKey + ", Value: " + value;
-  }
-  
+ 
